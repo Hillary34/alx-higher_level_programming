18c3ca7c7af24e55efdf031704a7e57e0cbbc422
Hillary34 <ezemahillary34@gmail.com>
2022-09-25 06:36:40 -0700

    my commit

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("Holberton School")
 def uppercase(str):
 for i in str:
 if ord(i) >= 97 and ord(i) <= 122:
-i = chr(ord(i) - 32)
-print("{}".format(i), end="")
-print()
+   i = chr(ord(i) - 32)
+     print("{}".format(i), end="")
+        print("Holberton School")
+          print()
